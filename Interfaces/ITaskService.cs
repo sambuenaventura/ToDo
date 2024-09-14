7d4e0bc361fe48f7ca516de299907c76f357a332
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-09-15 02:20:33 +0800

    feat: Add ITaskService interface for task operations

---
+++ Interfaces/ITaskService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Task;
+
+namespace api.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<List<TaskDto>> GetAllTasksAsync();
+        Task<TaskDto> GetTaskByIdAsync(int id);
+        Task<TaskDto> CreateTaskAsync(CreateTaskDto createTaskDto);
+        Task<TaskDto> UpdateTaskAsync(int id, UpdateTaskDto updateTaskDto);
+        Task<bool> DeleteTaskAsync(int id);
+    }
+}
