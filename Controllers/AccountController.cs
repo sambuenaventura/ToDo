40cf2f803f22db311fd4db2751b7a838c9d1e4e4
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-18 17:37:31 +0800

    feat: Enhance AccountController with token generation on registration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using api.Dtos.Account;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using api.Interfaces;
 
 namespace api.Controllers
 {
@@ -14,10 +15,12 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
@@ -41,7 +44,14 @@ namespace api.Controllers
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if (roleResult.Succeeded)
                     {
-                        return Ok("User created");
+                        return Ok(
+                            new NewUserDto
+                            {
+                                UserName = appUser.UserName,
+                                Email = appUser.Email,
+                                Token = _tokenService.CreateToken(appUser)
+                            }
+                        );
                     }
                     else
                     {
