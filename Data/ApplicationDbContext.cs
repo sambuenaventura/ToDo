5735249869ecaf147bfcff6ed1deea8400f30a5d
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-10-18 17:29:42 +0800

    feat: Add migration for seeding roles in the database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Task = api.Models.Task; // Alias the Task model
 
 namespace api.Data
 {
@@ -19,63 +21,58 @@ namespace api.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Models.Task>(entity => 
+            List<IdentityRole> roles = new List<IdentityRole>
             {
-                entity.HasKey(e => e.Id);
-
-                entity.Property(e => e.Name)
-                .IsRequired()
-                .HasMaxLength(255);
-
-                entity.Property(e => e.Description)
-                .HasDefaultValue(string.Empty);
-
-                entity.Property(e => e.Due)
-                .IsRequired();
-
-                entity.Property(e => e.Priority)
-                .HasConversion<string>();
-
-                entity.Property(e => e.Status)
-                .HasConversion<string>();
-
-                entity.Property(e => e.Tag)
-                .HasMaxLength(100)
-                .IsRequired();
-
-                entity.Property(e => e.Attachment)
-                .HasMaxLength(255); 
-
-            });
-        }
-
-        public override int SaveChanges()
-        {
-            SetTimestamps();
-            return base.SaveChanges();
-        }
-
-        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, System.Threading.CancellationToken cancellationToken = default)
-        {
-            SetTimestamps();
-            return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-        }
-
-        private void SetTimestamps()
-        {
-            var entries = ChangeTracker.Entries()
-                .Where(e => e.Entity is BaseEntity && (e.State == EntityState.Added || e.State == EntityState.Modified));
-
-            foreach (var entry in entries)
-            {
-                var entity = (BaseEntity)entry.Entity;
-
-                if (entry.State == EntityState.Added)
+                new IdentityRole
                 {
-                    entity.CreatedAt = DateTime.UtcNow;
-                }
-                entity.UpdatedAt = DateTime.UtcNow;
-            }
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                },
+            };
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
         }
+
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     base.OnModelCreating(modelBuilder);
+
+        //     modelBuilder.Entity<Models.Task>(entity => 
+        //     {
+        //         entity.HasKey(e => e.Id);
+
+        //         entity.Property(e => e.Name)
+        //         .IsRequired()
+        //         .HasMaxLength(255);
+
+        //         entity.Property(e => e.Description)
+        //         .HasDefaultValue(string.Empty);
+
+        //         entity.Property(e => e.Due)
+        //         .IsRequired();
+
+        //         entity.Property(e => e.Priority)
+        //         .HasConversion<string>();
+
+        //         entity.Property(e => e.Status)
+        //         .HasConversion<string>();
+
+        //         entity.Property(e => e.Tag)
+        //         .HasMaxLength(100)
+        //         .IsRequired();
+
+        //         entity.Property(e => e.Attachment)
+        //         .HasMaxLength(255); 
+
+        //     });
+        // }
+
     }
 }
---
+++ Migrations/20241018092736_SeedRole.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "152deea5-37a3-4e85-92a8-ff54f23a71de");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ffa34a2f-ad0e-45a0-8f50-fa2205380070");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3211c480-5545-44d6-b43b-cef433a37469", "0f79d2a8-b802-42da-a121-f16e6530230a", "User", "USER" },
+                    { "426e637a-4fee-446c-8e13-b4c6dd49cba9", "03295547-d764-47b8-a4d9-68ce8140a3a5", "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3211c480-5545-44d6-b43b-cef433a37469");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "426e637a-4fee-446c-8e13-b4c6dd49cba9");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "152deea5-37a3-4e85-92a8-ff54f23a71de", "88457b56-1f49-4f27-8bda-079cd997dd23", "Admin", "ADMIN" },
+                    { "ffa34a2f-ad0e-45a0-8f50-fa2205380070", "55503411-fe24-449d-99e9-e95482ea4e6e", "User", "USER" }
+                });
+        }
+    }
+}
